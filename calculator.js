f75c5d66a66f49436a523cd2cbd49ecbf6e76da3
ABHISHEK SINGH <64076762+abk90133@users.noreply.github.com>
2020-05-28 11:49:51 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(request, response) {
+  var num1 = Number(request.body.num1);
+  var num2 = Number(request.body.num2);
+
+  var result = num1 + num2;
+  response.send("The Result is: " + result);
+});
+
+app.listen(2000, function() {
+  console.log("Started at Port");
+});
